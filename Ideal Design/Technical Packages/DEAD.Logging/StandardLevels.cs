fb634936a0b3b90ac9209c9b74a314f4e18a0e83
waywa <waywa@WAYWAW530W10>
2016-08-29 20:13:03 +0800

    delete packages

---
+++ Ideal Design/Technical Packages/DEAD.Logging/StandardLevels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEAD.Logging
+{
+	public enum StandardLevels
+	{
+		Critical,
+		Important,
+		Warning,
+		Infomation,
+		Debug,
+
+	}
+}
